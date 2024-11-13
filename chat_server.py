503c32cdd04250526a88440a66a908c17fca3477
MichaleLord <user004sh@gmail.com>
2024-11-13 18:49:27 +0200

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -18,4 +18,4 @@ def handle_message(data):
     send(data, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=port)
